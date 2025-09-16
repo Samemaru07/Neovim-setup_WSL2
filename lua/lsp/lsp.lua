a0ab2b4a5fb08e6523f2ab32c318ce880f8f79ca
Samemaru07 <osame0830@icloud.com>
2025-09-16 18:53:25 +0900

    reafctor: 可読性向上の為、余分な改行を削除したり書き方を変更

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -8,28 +8,27 @@ require("mason-lspconfig").setup({
         "pyright",
         "html",
         "cssls",
-        "ts_ls",     -- ts_ls に統一
-        "jsonls",
-    },
+        "ts_ls",
+        "jsonls"
+    }
 })
 
--- 共通 on_attach
 local on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
         buffer = bufnr,
         callback = function()
             vim.lsp.buf.format({ bufnr = bufnr })
-        end,
+        end
     })
 end
 
--- capabilities の設定
+-- capabilities
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- lspconfig を使う
+-- lspconfig
 local lspconfig = require("lspconfig")
 
--- Lua LS は特別設定
+-- Lua LS
 lspconfig.lua_ls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
@@ -38,28 +37,28 @@ lspconfig.lua_ls.setup({
             runtime = { version = "LuaJIT" },
             workspace = {
                 library = { vim.env.VIMRUNTIME },
-                checkThirdParty = false,
+                checkThirdParty = false
             },
             diagnostics = {
-                globals = { "vim" },
+                globals = { "vim" }
             },
             format = {
                 enable = true,
                 defaultConfig = {
                     indent_style = "space",
-                    indent_size = "4",
-                },
-            },
-        },
-    },
+                    indent_size = "4"
+                }
+            }
+        }
+    }
 })
 
--- その他の言語サーバー（共通設定）
+-- その他の言語サーバ
 local servers = { "clangd", "pyright", "html", "cssls", "ts_ls", "jsonls" }
 
 for _, server in ipairs(servers) do
     lspconfig[server].setup({
         on_attach = on_attach,
-        capabilities = capabilities,
+        capabilities = capabilities
     })
 end
