553abbadf9736d87017777c76fb95852d3ab0311
Samemaru07 <osame0830@icloud.com>
2025-09-29 17:29:14 +0900

    feat: UPDATE文の整形を正しく行う為、自作フォーマッタを導入

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -51,13 +51,15 @@ local delete_sql_formatter = {
             sql = vim.trim(sql)
 
             sql = sql:gsub("[Dd][Ee][Ll][Ee][Tt][Ee]", "DELETE")
+            sql = sql:gsub("[Ff][Rr][Oo][Mm]", "FROM")
 
-            sql = sql:gsub("DELETE%s+", "DELETE\n", 1)
+            sql = sql:gsub("DELETE%s*FROM%s*(%w+)", "DELETE\nFROM\n    %1")
 
             return { { text = sql } }
         end
     }
 }
+
 
 local pg_format = null_ls.builtins.formatting.pg_format.with({
     to_stdin = true,
