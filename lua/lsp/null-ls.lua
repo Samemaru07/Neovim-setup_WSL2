cd774f487e148ced92b52fddb8c5eb3691357033
Samemaru07 <osame0830@icloud.com>
2025-09-29 23:21:47 +0900

    feat: update_sql_formatterの改行系を修正

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -24,7 +24,8 @@ local update_sql_formatter = {
                         end
                         return "SET\n" .. table.concat(parts, ",\n")
                     end)
-                sql = vim.trim(before_where) .. "\n" .. where_clause
+                where_clause = vim.trim(where_clause):gsub("WHERE", "WHERE\n    ")
+                sql = before_where .. "\n" .. where_clause
             else
                 sql = sql
                     :gsub("SET%s*(.-)$", function(assignments)
