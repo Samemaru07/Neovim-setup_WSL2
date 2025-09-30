f1aec1670e4999d1f48a677d3a168f09eeee61bf
Samemaru07 <osame0830@icloud.com>
2025-09-30 15:23:14 +0900

    feat: pgFormatterを削除

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -1,5 +1,25 @@
 local null_ls = require("null-ls")
 
+local sql_formatter = {
+    name = "simple_sql_formatter",
+    method = null_ls.methods.FORMATTING,
+    filetypes = { "sql" },
+    generator = {
+        fn = function(params)
+            local sql = table.concat(params.content, "\n")
+            sql = vim.trim(sql)
+
+            sql = sql:gsub("UPDATE%s+(%w+)%s+SET", "UPDATE\n    %1\nSET")
+
+            sql = sql:gsub("DELETE%s+FROM%s+(%w+)", "DELETE\nFROM\n    %1")
+
+            sql = sql:gsub("%s+WHERE%s+", "\nWHERE\n    ")
+
+            return { { text = sql } }
+        end
+    }
+}
+
 local pg_format = null_ls.builtins.formatting.pg_format.with({
     to_stdin = true,
     extra_args = {
@@ -10,6 +30,6 @@ local pg_format = null_ls.builtins.formatting.pg_format.with({
 
 null_ls.setup({
     sources = {
-        pg_format
+        sql_formatter
     }
 })
