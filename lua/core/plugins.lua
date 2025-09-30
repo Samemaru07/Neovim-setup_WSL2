b1aa0c1529bcb2009ae3201f84febebfe9536358
Samemaru07 <osame0830@icloud.com>
2025-09-30 17:30:57 +0900

    feat: プラグインにpgFormatterを追加

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -178,5 +178,32 @@ require("lazy").setup({
         init = function()
             vim.g.mkdp_auto_start = 0
         end
+    },
+
+    -- pgFormatter
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local null_ls = require("null-ls")
+
+            local pg_format = null_ls.builtins.formatting.pg_format.with({
+                command = "/usr/local/bin/pg_format",
+                to_stdin = true,
+                filetypes = { "sql", "pgsql" },
+                extra_args = {
+                    "--format", "text",
+                    "--no-rcfile",
+                    "--keyword-case", "2",
+                    "--type-case", "2",
+                    "--spaces", "4"
+                }
+            })
+
+            null_ls.setup({
+                debug = true,
+                sources = { pg_format }
+            })
+        end
     }
 })
