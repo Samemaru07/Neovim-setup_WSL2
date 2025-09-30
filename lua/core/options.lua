e611aaf04ae95471f33118b5a51b3c1762578846
Samemaru07 <osame0830@icloud.com>
2025-09-30 19:27:57 +0900

    refactor: コメントアウト・改行を整理

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,18 +3,14 @@ vim.o.relativenumber = false
 vim.opt.clipboard = "unnamedplus"
 vim.o.termguicolors = true
 
--- =========================================
--- インデント設定 (Tab = 4スペース)
--- =========================================
-vim.o.tabstop = 4        -- 画面上でのタブ幅
-vim.o.shiftwidth = 4     -- 自動インデントの幅
-vim.o.softtabstop = 4    -- TabキーやBackspaceの挙動を4スペースに
-vim.o.expandtab = true   -- タブをスペースに変換
-vim.o.smartindent = true -- 新しい行で自動インデント
+-- インデント設定
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
 
--- =========================================
 -- Lazy.nvim setup
--- =========================================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     print("Lazy.nvim not found. Please run git clone.")
@@ -26,7 +22,6 @@ vim.opt.shellcmdflag = "-c"
 vim.opt.shellquote = ""
 vim.opt.shellxquote = ""
 vim.opt.showmode = false
-
 
 -- 改行コード
 vim.opt.fileformat = "unix"
