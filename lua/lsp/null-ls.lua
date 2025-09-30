e914f4d18c6db033737f5586e26f5d7b104d24e0
Samemaru07 <osame0830@icloud.com>
2025-09-30 16:23:54 +0900

    feat: pgFormatterのオプションを修正

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -4,14 +4,15 @@ local pg_format = null_ls.builtins.formatting.pg_format.with({
     to_stdin = true,
     filetypes = { "sql", "pgsql" },
     extra_args = {
+        "--format", "text",
+        "--no-rcfile",
         "--keyword-case", "2",
+        "--type-case", "2",
         "--spaces", "4"
     }
 })
 
 null_ls.setup({
     debug = true,
-    sources = {
-        pg_format
-    }
+    sources = { pg_format }
 })
