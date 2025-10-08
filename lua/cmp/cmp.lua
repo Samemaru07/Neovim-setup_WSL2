49a49df831f5447a8f8c098a36864bb9c506fa43
Samemaru07 <osame0830@icloud.com>
2025-10-08 20:33:07 +0900

    feat: <CR>のマッピングを復元

--- lua/cmp/cmp.lua
+++ lua/cmp/cmp.lua
@@ -1,13 +1,10 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
--- lazy.nvim 経由で friendly-snippets をロード
-require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load({
+    paths = { "C:/Users/nakayama/AppData/Local/nvim-data/lazy/friendly-snippets" }
+})
 
-require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/lua/snippets" })
-
-
--- JS 用 clg スニペットを直接追加
 luasnip.add_snippets("javascript", {
     luasnip.snippet("clg", {
         luasnip.text_node("console.log("),
@@ -22,19 +19,11 @@ cmp.setup({
             luasnip.lsp_expand(args.body)
         end
     },
-    mapping = {
+    mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
@@ -45,11 +34,20 @@ cmp.setup({
             else
                 fallback()
             end
-        end, { "i", "s" })
-   },
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
     sources = cmp.config.sources({
-        { name = "nvim_lsp", keyword_length = 1 }, -- 型補完
-        { name = "luasnip",  keyword_length = 1 }, -- スニペット補完
+        { name = "nvim_lsp", keyword_length = 1 },
+        { name = "luasnip",  keyword_length = 1 },
     }, {
         { name = "buffer" },
         { name = "path" }
