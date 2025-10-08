b7a56408f73cccc9aae9111ccf41fd7a27d45285
Samemaru07 <osame0830@icloud.com>
2025-10-09 01:24:50 +0900

    feat: Reactの自作スニペットを修正

--- lua/snippets/typescriptreact.lua
+++ lua/snippets/typescriptreact.lua
@@ -5,31 +5,14 @@ local i = ls.insert_node
 
 return {
     s("myrafce", {
-        t({
-            "import React from 'react'",
-            "",
-            "interface Props {",
-            "    ",
-        }),
-        i(2),
-        t({
-            "}",
-            "",
-            "export const ",
-        }),
-        i(1, "ComponentName"),
-        t({
-            ": React.FC<Props> = () => {",
-            "    return (",
-            "        <div>",
-            "            "
-        }),
-        i(0),
-        t({
-            "        </div>",
-            "    )",
-            "}"
-        })
+        t("const "), i(1, "Component"), t(" = () => {"),
+        t({ "", "    return (" }),
+        t({ "", "        <div>" }),
+        t({ "", "            " }), i(2),
+        t({ "", "        </div>" }),
+        t({ "", "    );" }),
+        t({ "", "};" }),
+        t({ "", "", "export default " }), i(1)
     }),
 
     s("us", {
