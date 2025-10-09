ba69df8fec505f083c01043703f2d5c60c945468
Samemaru07 <osame0830@icloud.com>
2025-10-09 22:04:10 +0900

    feat: rafceを修正

--- lua/snippets/typescriptreact.lua
+++ lua/snippets/typescriptreact.lua
@@ -3,16 +3,10 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-local function get_component_name_from_filename()
-    local filename = vim.fn.expand('%:t:r')
-    filename = filename:gsub("[-_](%w)", string.upper)
-    return filename:gsub("^%l", string.upper)
-end
-
 return {
     s("rafce", {
         t("function "),
-        i(1, get_component_name_from_filename()),
+        i(1, "ComponentName"),
         t("() {"),
         t({ "", "    return (" }),
         t({ "", "        <>" }),
