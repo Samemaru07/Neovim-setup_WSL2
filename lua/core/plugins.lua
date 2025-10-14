672732495a89e0c96c4b08311c03167789610d78
Samemaru07 <osame0830@icloud.com>
2025-10-14 14:06:13 +0900

    feat: texの保存時の自動コンパイルを設定

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -296,3 +296,10 @@ require("lazy").setup({
     { "mfussenegger/nvim-lint" },
     { "stevearc/conform.nvim" }
 })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.tex",
+    callback = function()
+        vim.cmd("VimtexCompile")
+    end,
+})
