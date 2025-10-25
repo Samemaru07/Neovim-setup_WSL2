e3c56c2593d8075423e96f22be0e965043a779de
Samemaru07 <osame0830@icloud.com>
2025-10-25 16:37:05 +0900

    feat: LSPキーマップを追加

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -17,7 +17,30 @@ require("mason-lspconfig").setup({
     },
 })
 
-local on_attach = function(client, bufnr) end
+local on_attach = function(client, bufnr)
+    local map = vim.keymap.set
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+
+    map("n", "K", vim.lsp.buf.hover, opts)
+    map("n", "gd", vim.lsp.buf.definition, opts)
+    map("n", "gr", vim.lsp.buf.references, opts)
+    map("n", "gi", vim.lsp.buf.implementation, opts)
+    map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+    map("n", "[d", vim.diagnostic.goto_prev, opts)
+    map("n", "]d", vim.diagnostic.goto_next, opts)
+    map("n", "<leader>dl", vim.diagnostic.open_float, opts)
+
+    if client.name == "tsserver" then
+        map(
+            "n",
+            "<leader>oi",
+            ":OrganizeImports<CR>",
+            { noremap = true, silent = true, buffer = bufnr, desc = "Organize Imports" }
+        )
+    end
+end
 
 -- capabilities
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
