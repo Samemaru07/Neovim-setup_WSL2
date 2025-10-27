06066d147445d2e2bd9e9ecb3a8a09570f78e246
Samemaru07 <osame0830@icloud.com>
2025-10-27 21:52:30 +0900

    feat: nvim-notifyを導入

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -409,4 +409,10 @@ require("lazy").setup({
             })
         end,
     },
+    {
+        "rcarriga/nvim-notify",
+        config = function()
+            vim.notify = require("notify")
+        end,
+    },
 })
