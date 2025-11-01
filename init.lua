794a33e3a40a17e25351193875b84f781a12362d
Samemaru07 <osame0830@icloud.com>
2025-11-01 21:38:31 +0900

    feat: win32yank.exeを導入

--- init.lua
+++ init.lua
@@ -79,14 +79,14 @@ vim.api.nvim_create_autocmd("FileChangedShellPost", {
 
 if vim.fn.has("wsl") == 1 then
     vim.g.clipboard = {
-        name = "WslClipboard",
+        name = "win32yank-wsl",
         copy = {
-            ["+"] = "clip.exe",
-            ["*"] = "clip.exe",
+            ["+"] = "win32yank.exe -i --crlf",
+            ["*"] = "win32yank.exe -i --crlf",
         },
         paste = {
-            ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ["+"] = "win32yank.exe -o --lf",
+            ["*"] = "win32yank.exe -o --lf",
         },
         cache_enabled = 0,
     }
