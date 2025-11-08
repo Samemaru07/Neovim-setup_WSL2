c1543776f410309a2bdcf61f326f6a139b476306
Samemaru07 <osame0830@icloud.com>
2025-11-08 20:02:33 +0900

    feat: Normal/Visualモードで単語移動のEを%に割り当て

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -151,3 +151,6 @@ vim.api.nvim_create_autocmd("User", {
         vim.fn["skkeleton#register_keymap"]("input", "Q", "hankatakana")
     end,
 })
+
+vim.keymap.set("n", "E", "%", { noremap = true, silent = true })
+vim.keymap.set( "v", "E", "%", { noremap = true, silent = true })
