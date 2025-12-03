9c0c4721bd771c0f7fe446bab2427039889c209a
Samemaru07 <osame0830@icloud.com>
2025-12-03 20:16:14 +0900

    feat: Telescopeを高速化

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -263,6 +263,7 @@ require("lazy").setup({
         "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
+            require("telescope").load_extension("fzf")
             require("telescope").setup({
                 defaults = { file_ignore_patterns = { "node_modules", ".git/" } },
             })
