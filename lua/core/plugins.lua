0e11cc2e09a43aa8da8eeb6c50e1e31bf7cb8aad
Samemaru07 <osame0830@icloud.com>
2025-12-09 22:59:27 +0900

    feat: nvim-ts-autotagを導入。タグの自動リネームが可能に

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -811,4 +811,10 @@ require("lazy").setup({
             })
         end,
     },
+    {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
+    },
 })
