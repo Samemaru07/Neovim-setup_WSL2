e465aa087196d52f1a0f3bec8f29dae3ba88990e
Samemaru07 <osame0830@icloud.com>
2025-12-10 18:42:38 +0900

    feat: flash.nvimを導入

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -817,4 +817,27 @@ require("lazy").setup({
             require("nvim-ts-autotag").setup()
         end,
     },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "s",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").jump()
+                end,
+                desc = "Flash",
+            },
+            {
+                "S",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").treesitter()
+                end,
+                desc = "Flash Treesitter",
+            },
+        },
+    },
 })
