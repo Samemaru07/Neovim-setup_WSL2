bdc7f452c08b126a6ceb9f8ae0e3d5c4cf435da0
Samemaru07 <osame0830@icloud.com>
2026-01-25 17:09:52 +0900

    fix: :MarkdownPreviewが開けなかったバグを修正。IPアドレスを固定。

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -375,7 +375,8 @@ require("lazy").setup({
         build = "cd app && npm install",
         init = function()
             vim.g.mkdp_auto_start = 0
-            vim.g.mkdp_open_to_the_world = 1
+            vim.g.mkdp_open_to_the_world = 0
+            vim.g.mkdp_open_ip = "127.0.0.1"
             vim.g.mkdp_browserfunc = "OpenWslBrowser"
             vim.cmd([[
             function! OpenWslBrowser(url)
